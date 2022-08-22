4809fa40a5635c7b3c89746a01e3db90d1b7a670
Himanshu Dangre <himanshudangre99@gmail.com>
2022-08-22 11:19:46 +0530

    first commit

---
+++ itemarray.js
@@ -0,0 +1,12 @@
+const itemarray=[{
+    "itemname":"Black coffee",
+    "price":20,
+    "quantity":10
+}]
+
+const itemarray=[{
+    "itemname":"Espresso",
+    "price":30,
+    "quantity":12
+}]
+
---
+++ main.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router=express.Router()
+var app=express()
+
+router.get("/viewitem",(req,res)=> {
+
+})
+
+router.get("/viewitem",(req,res)=> {
+    
+})
+
+router.patch("/viewitem",(req,res)=> {
+    
+})
+
+router.delete("/viewitem",(req,res)=> {
+    
+})
+
+router.get("/viewitem",(req,res)=> {
+    
+})
